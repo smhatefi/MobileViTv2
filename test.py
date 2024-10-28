96d9592ec3ac508f956c897887fda8e1d0dd82c8
smhatefi <smhatefi75@gmail.com>
2024-10-28 23:42:57 +0330

    files modified

--- test.py
+++ test.py
@@ -7,11 +7,11 @@ os.environ["KERAS_BACKEND"] = "jax"
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-from mobile_vit_v2 import build_MobileViT_v2
+from mobilevit_v2 import build_MobileViT_v2
 
 
 # Load the labels (ImageNet class names)
-os.system("!wget -q -O imagenet_classes.txt https://raw.githubusercontent.com/pytorch/hub/master/imagenet_classes.txt")
+os.system("wget -q -O imagenet_classes.txt https://raw.githubusercontent.com/pytorch/hub/master/imagenet_classes.txt")
 with open('imagenet_classes.txt') as f:
     labels = [line.strip() for line in f.readlines()]
 
